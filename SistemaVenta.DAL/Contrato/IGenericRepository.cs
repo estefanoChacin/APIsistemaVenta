59052a039f26aeaeee70420a00b0133c6a2fdd29
estefano_chacin <estefanochacin@wposs.com>
2024-09-17 12:34:48 -0500

    Agregar archivos de proyecto.

---
+++ SistemaVenta.DAL/Contrato/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Contrato
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Obtener(Expression<Func<T, bool>> filtro);
+        Task<T> Crear(T modelo);
+        Task<bool> Editar(T modelo);
+        Task<bool> Elimiar(T modelo);
+        Task<IQueryable<T>> Consultar(Expression<Func<T, bool>> filtro = null);
+
+    }
+}
