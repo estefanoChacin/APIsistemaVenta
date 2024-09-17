fc0c87525a9a776b3813ded879b6bdf491d09a1e
estefano_chacin <estefanochacin@wposs.com>
2024-09-17 12:40:54 -0500

    correcion en el nombre del metodo de eliminar de la capa DAL

--- SistemaVenta.DAL/Contrato/IGenericRepository.cs
+++ SistemaVenta.DAL/Contrato/IGenericRepository.cs
@@ -12,7 +12,7 @@ namespace SistemaVenta.DAL.Contrato
         Task<T> Obtener(Expression<Func<T, bool>> filtro);
         Task<T> Crear(T modelo);
         Task<bool> Editar(T modelo);
-        Task<bool> Elimiarr(T modelo);
+        Task<bool> Eliminar(T modelo);
         Task<IQueryable<T>> Consultar(Expression<Func<T, bool>> filtro = null);
 
     }
